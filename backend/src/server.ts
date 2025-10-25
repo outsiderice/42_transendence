7852d6a3fadd00c289a283c6c9b22363183f5b37
Jose casa <josemiguel.profe@gmail.com>
2025-10-25 11:43:48 +0200

    simple-swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,32 @@
 import Fastify from 'fastify';
 import {usersRoutes} from './modules/users/usersRoutes';
+import Swagger from "@fastify/swagger";
+import SwaggerUI from "@fastify/swagger-ui";
 
 const app = Fastify({logger : true});
 
 
 const start = async () => {
+  await app.register(Swagger, {
+  openapi: {
+    info: {
+      title: "API Example",
+      version: "1.0.0",
+      },
+    },
+  });
+
+await app.register(SwaggerUI, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+    },
+  });
   await usersRoutes(app);
   await app.listen({ port: 3000 }).then(() => {
     console.log('Server is running on http://localhost:3000');
   });
+  
 };
 start();
