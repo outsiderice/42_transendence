651069728722a5206294b1fb09c9bceaeee619af
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-11-25 17:12:27 +0000

    new endpoint which requires id

--- backend/src/modules/users/usersRoutes.ts
+++ backend/src/modules/users/usersRoutes.ts
@@ -1,13 +1,9 @@
 import {FastifyInstance} from 'fastify';
 import {usersController} from './usersControllers';
+import {usersController2} from './usersControllers';
 
 export const usersRoutes = (app: FastifyInstance) => {
   app.get('/users', usersController);
-
-  app.get('/moco', () => {
-    return { users: "moco" };
-
-  });
 
   app.get<{Params: {id: number}}>('/testy/:id', {handler: usersController2});
 
