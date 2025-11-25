7c2f8cb4c26b91e1ced14e32f14cdb476721baa3
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-11-25 16:11:54 +0000

    testing how fastify works

--- backend/src/modules/users/usersControllers.ts
+++ backend/src/modules/users/usersControllers.ts
@@ -1,4 +1,13 @@
- export const usersController = () => {
-    return { users: "users" };
+import { FastifyReply, FastifyRequest } from "fastify";
 
-  }
+export const usersController = (req: FastifyRequest, res: FastifyReply) => {
+  return { users: 'users' }
+
+};
+
+export const usersController2 = (req: FastifyRequest<Params: <{Params: {id: number}}>, res: FastifyReply) => {
+  
+  const params = req.params;
+
+  return { users: params.id }
+};
--- backend/src/modules/users/usersRoutes.ts
+++ backend/src/modules/users/usersRoutes.ts
@@ -2,11 +2,13 @@ import {FastifyInstance} from 'fastify';
 import {usersController} from './usersControllers';
 
 export const usersRoutes = (app: FastifyInstance) => {
-  app.get('/users',usersController);
+  app.get('/users', usersController);
 
   app.get('/moco', () => {
     return { users: "moco" };
 
   });
+
+  app.get<{Params: {id: number}}>('/testy/:id', {handler: usersController2});
 
 };
