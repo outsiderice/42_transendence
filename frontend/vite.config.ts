b37005ee413995f929f589190f354d4810db9a0c
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-12-05 17:22:32 +0000

    default values

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-const PORT = Number(process.env.VITE_PORT);
-const HOST = process.env.VITE_HOST;
+const PORT = Number(process.env.VITE_PORT) || 8080;
+const HOST = process.env.VITE_HOST || '0.0.0.0';
 
 export default defineConfig({
   plugins: [
