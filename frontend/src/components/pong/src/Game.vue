2ff223665c2932fec7427d27fd59ed07231c3b34
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-12-18 19:51:48 +0100

    trying reactive image no success

--- frontend/src/components/pong/src/Game.vue
+++ frontend/src/components/pong/src/Game.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts"> 
+    import { GameCanvas } from "./GameCanvas.vue"
+    import { Pong } from "./Pong.ts"
+    import { reactive } from 'vue'
 
-//
+    const game = new Pong(800, 600);
+
+    const game_state = reactive(game.getGameState());
+
 </script>
-
 
 <template>
     <h1> "hey bitches kjdkfjfj" </h1>
-    <!--<GameCanvas  /> -->
+    <GameCanvas :gameState="game_state"> </GameCanvas>
  </template>
---
+++ frontend/src/components/pong/src/GameCanvas.vue
@@ -0,0 +1,19 @@
+
+<script setup lang="ts">
+    import { GameState } from "./GameState.ts";
+    const   props = defineProps({
+        gameState: GameState,
+
+    });
+    const   canvas = document.getElementById("myCanvas");
+    const   renderer = new Renderer(canvas);
+    renderer.draw(props);
+
+</script>
+
+
+<template>
+
+    <canvas id="myCanvas" width="800" height="600"></canvas>
+
+ </template>
