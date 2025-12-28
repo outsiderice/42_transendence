944919639fb95b5dbd9d1c3c86a2f575814953c2
Andrea Martins <andmart2@cbr2s6.42barcelona.com>
2025-12-28 14:11:43 +0100

    pong-link component finished

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HomeTitle from './components/HomeTitle.vue'
+import PongLink from './components/PongLink.vue'
 </script>
 
 <template>
 	<HomeTitle />
 	<RouterView />
 	<footer class="absolute bottom-0 block w-full mb-[2rem] flex justify-center gap-[2rem]" >
-		<a 
-			href="/terms_of_service" 
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			Trems of service
-		</a>
-		<a
-			href="/privacy_policy"	
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
+		<PongLink to="/terms_of_service">
+      		Terms of service
+    	</PongLink>
+		<PongLink to="/privacy_policy">
 			Privacy policy
-		</a>
-		<a
-			href="/test"	
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			components tests
-		</a>
+    	</PongLink>
+		<PongLink to="/test">
+			Test
+    	</PongLink>
 	</footer>
 </template>
 
---
+++ frontend/src/components/PongLink.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    interface Props {
+        href: string
+    }
+
+    defineProps<Props>()
+</script>
+
+<template>
+    <a 
+            :href= "href"
+            class="pong-link text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
+    >
+            <slot />   
+    </a>
+</template>
+
+<style scoped>
+    .pong-link {
+        text-decoration-thickness: 1.5px; 
+    }
+
+
+    .pong-link:hover {
+        text-decoration-thickness: 4px;
+    }
+</style>
