75b56a60549c1e4b928cc603e1e9a307a5aced54
A.M.S <99796232+outsiderice@users.noreply.github.com>
2026-01-09 19:26:14 +0000

    added pageauth decorate too

--- backend/src/plugins/jwt.plugin.ts
+++ backend/src/plugins/jwt.plugin.ts
@@ -1,6 +1,27 @@
-import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import fp from 'fastify-plugin';
 import jwt from '@fastify/jwt';
+
+/** 
+LIST OF places where jwt plugin needs to be used as prehandler:
+authenticate:
+[] - POST /api/auth/logout
+[] - PUT /api/me
+[] - GET /api/me
+[] - GET /api/users
+[] - GET /api/users/:id
+[] - POST /api/users/:id/add-friend
+[] - DELETE /api/users/:id/remove-friend
+[] - POST /api/games
+
+pageauthenticate:
+[] - GET /play-online
+[] - GET /dashboard
+[] - GET /settings
+[] - GET /users
+[] - GET /users/:id
+ */
+
 
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(jwt, {
@@ -12,10 +33,19 @@ export default fp(async function (fastify: FastifyInstance) {
       // This will verify the JWT and populate request.user
       await request.jwtVerify();
     } catch (err) {
-      reply.code(401).send({
+      reply.code(401).send({        //redirect to login or send error?
         error: 'Authentication required',
         message: 'Please provide a valid token'
       });
+    }
+  })
+
+  fastify.decorate("authenticatePage", async function(request: FastifyRequest, reply: FastifyReply) {
+  try {
+      // This will verify the JWT and populate request.user
+      await request.jwtVerify();
+    } catch (err) {
+      reply.redirect('/sign-in');
     }
   })
 })
