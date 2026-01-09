1d3597ad2572606e141e1bb23151ddcd8c7507a4
Andrea Martins <andmart2@cbr1s3.42barcelona.com>
2026-01-09 20:00:13 +0100

    pong links colors working when specifies in style of footer

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,35 +1,29 @@
 <script setup lang="ts">
+import PongLink from '../components/PongLink.vue'
 </script>
 
 <template>
-	<footer class="fixed bottom-0 block w-full mb-[2rem] flex justify-center gap-[2rem]" >
-		<a 
-			href="/terms_of_service" 
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			Trems of service
-		</a>
-		<a
-			href="/privacy_policy"	
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			Privacy policy
-		</a>
-		<a
-			href="/test"
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			components tests
-		</a>
-		<a
-			href="/test_2"
-			class="text-blue-600 underline decoration-1 hover:decoration-2 visited:text-indigo-700"
-		>
-			components tests 2
-		</a>
-	</footer>
+	<footer class="fixed bottom-0 block w-full mb-[2rem] flex justify-center gap-[2rem]">
+	<PongLink href="/terms_of_service" class="underline decoration-1 hover:decoration-2">
+		Terms of service
+	</PongLink>
+	<PongLink href="/privacy_policy" class="underline decoration-1 hover:decoration-2">
+		Privacy policy
+	</PongLink>
+	<PongLink href="/test" class="underline decoration-1 hover:decoration-2">
+		Components test
+	</PongLink>
+	<PongLink href="/test_2" class="underline decoration-1 hover:decoration-2">
+		Components test 2
+	</PongLink>
+</footer>
+
 </template>
 
-<style scoped>
+<style>
+
+.pong-link:visited {
+  color: var(--color_accent_link_visited);
+}
 
 </style>
