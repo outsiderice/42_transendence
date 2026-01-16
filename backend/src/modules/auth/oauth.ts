e4edc188184eff939f592aaa47c2264fdded58cc
A.M.S <99796232+outsiderice@users.noreply.github.com>
2026-01-16 18:07:47 +0000

    deleted unused oauth file

--- backend/src/modules/auth/oauth.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-//callback route for oauth providers
-export async function oauthRoutes(fastify: FastifyInstance) {
-//get token and user info from github
-//populate request.user with user info
-//check if user exists in database
-//then call login or register logic
-}
