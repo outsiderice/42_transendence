c5db8d9922ef6f8caefe98e4e3941a498168a23c
Taha Tahere <elberkani.taha@gmail.com>
2026-01-16 20:01:35 +0100

    added the game historic card

--- frontend/src/views/TestView_2.vue
+++ frontend/src/views/TestView_2.vue
@@ -2,12 +2,13 @@
 import UserCard from "@/components/UserCard.vue";
 import DonutChart from "@/components/DonutChart.vue";
 import GameStatisticsCard from "@/components/GameStatisticsCard.vue";
+import GameHistoricStatisticsCard from "@/components/GameHistoricStatisticsCard.vue";
 </script>
 <!--
 	https://cdn.intra.42.fr/users/3652d353f132f5475c6b099ea0bdf1f2/tatahere.png
 -->
 <template>
-<section class="max-w-[80rem] p-[1rem] m-auto flex flex-col gap-[2rem]">
+<section class="max-w-[60rem] p-[1rem] m-auto flex flex-col gap-[2rem]">
 	<h3 class="text-center mb-[4rem] text-[2rem]" >test page.</h3>
 	<h4>testing the user cards</h4>
 	<UserCard />
@@ -36,6 +37,14 @@ import GameStatisticsCard from "@/components/GameStatisticsCard.vue";
 	<GameStatisticsCard player2="taha" dateStr="31/10/2003" :pointsPlayer1="12" :pointsPlayer2="4" />
 	<GameStatisticsCard player2="pedro" dateStr="31/10/2003" :pointsPlayer1="11" :pointsPlayer2="12" />
 	<GameStatisticsCard player2="mark" dateStr="31/10/2003" :pointsPlayer1="11" :pointsPlayer2="12" />
+	<GameHistoricStatisticsCard 
+		:gamesWon="13" 
+		:gamesLost="3" 
+	/>
+	<GameHistoricStatisticsCard 
+		:gamesWon="7" 
+		:gamesLost="10" 
+	/>
 	<div />
 </section>
 </template>
