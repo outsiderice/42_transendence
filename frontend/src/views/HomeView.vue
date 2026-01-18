251636352960ad16af08c09a9f904f75d61ceb61
Taha Tahere <elberkani.taha@gmail.com>
2026-01-18 20:02:14 +0100

    progress of the day on adding buttons to the home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import PongToggleButton from '../components/PongToggleButton.vue'
+
+
+
 </script>
 
 <template>
-	<h3> home </h3>
+<section class="max-w-[60rem] p-[1rem] m-auto flex flex-col gap-[2rem]">
+	<PongToggleButton @click="hand"/>
+</section>
 </template>
