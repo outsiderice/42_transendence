5b7869e7afdd1b74033fa6e34ef6742c8f6deb42
Taha Tahere <elberkani.taha@gmail.com>
2026-01-19 21:24:06 +0100

    trying to log in to the backend

---
+++ frontend/notes.md
@@ -0,0 +1,4 @@
+to be able to fetch from the backe to the fronte end run the next command
+```bash
+chromium-browser --disable-web-security --user-data-dir="any directory"
+```
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,58 @@
 <script setup lang="ts">
-import PongToggleButton from '../components/PongToggleButton.vue'
+import { ref, onMounted } from 'vue';
+import PongButton from '../components/PongButton.vue';
 
+function contains_user(result_body: {username: string}[], name: string ) : boolean
+{
+	for (let i = 0; i < result_body.length; i++)
+	{
+		if (result_body[i].username == name)
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
+//	if the user is alredy created it dose nothing.
+async function	create_user(name: string)
+{
+	//	check if the user exists.
+	{
+		const response = await fetch("http://127.0.0.1:3001/api/users");
+		if (!response.ok)
+		{
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const result = await response.json();
+		console.log(result);
+		if (contains_user(result, name))
+		{
+			return ;
+		}
+	}
+	//	create the user.
+	{
+		// not able to figure out how to register a user.
+		const response = await fetch("http://127.0.0.1:3001/api/auth/register", {
+			method: 'POST',
+		});
+		console.log(response);
+	}
+}
+
+async function log_in_with_pedro()
+{
+	create_user("pedro");
+	
+}
 
 </script>
 
 <template>
 <section class="max-w-[60rem] p-[1rem] m-auto flex flex-col gap-[2rem]">
-	<PongToggleButton @click="hand"/>
+	<PongButton label="login with pedro"  @click="log_in_with_pedro"/>
+<!--	<PongButton label="login with juan"  @click="log_in_with_juan"/> -->
 </section>
 </template>
