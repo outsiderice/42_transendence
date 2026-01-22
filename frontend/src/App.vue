89ed8ca3af7e8c07516eed9f07ecf9fd190c3db9
Taha Tahere <elberkani.taha@gmail.com>
2026-01-22 17:13:26 +0100

    progress in the redirections

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HeaderComponent from './components/HeaderComponent.vue'
-import FooterComponent from './components/FooterComponent.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import {useRouter} from 'vue-router';
+
+//
+//	redirecting the user to the log in screen if it is not loged in.
+//
+
+function remove_traling_slash(path: string): string
+{
+	if (path[path.len - 1] === '/')
+	{
+		return path.slice(0, path.len - 1);
+	}
+	return path;
+}
+
+//	returns true if the path is both the signin one or the signup
+function is_on_login_screan(): boolean
+{
+	const path = remove_traling_slash(window.location.pathname);
+	if (path === "/")
+	{
+		return true;
+	}
+	return false;
+}
+
+	 
+
+
+
 </script>
 
 <template>
---
+++ frontend/src/views/UserListing.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<h3> userListing </h3>
+</template>
+
+<style scoped>
+
+</style>
