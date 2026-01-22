922883161146c39cce059c1098a84d08eeaa4c0c
Taha Tahere <elberkani.taha@gmail.com>
2026-01-22 19:38:57 +0100

    adding library to have jwt decoding.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { jwtDecode } from 'jwt-decode'
 
+const jwt = localStorage.getItem('accessToken');
 
+console.log(jwt);
+
+const decodedHeader = jwtDecode(jwt);
+console.log(decodedHeader);
 
 </script>
 
 <template>
-	<h3> home </h3>
-	<RouterLink to="/sign_in">Sign In</RouterLink>
-	<RouterLink to="/game">Play Game</RouterLink>
+	<h3>home page </h3>
 </template>
