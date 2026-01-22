a785522b09f2d79e79362c4b5ae840e59b24a1e6
outsiderice <arantxamagnelli@gmail.com>
2026-01-22 21:10:52 +0100

    rewrote game route

--- backend/src/modules/game/GameRoutes.ts
+++ backend/src/modules/game/GameRoutes.ts
@@ -1,56 +1,20 @@
-import { FastifyInstance } from "fastify";
-import { Game } from '../../services/dbClient';
+import { FastifyInstance, FastifyRequest } from "fastify";
 import {
-    getAllGamesController,
-    createGameController
+	GameController
 } from '.Controllers/gameControllers';
 
+export	const gameRoutes = async (app:FastifyInstance) => {
+	app.get<{
+		Querystring: {accessToken: string }
+	}>(
+		'ws/play',
+		{
+			websocket: true,
+			prehandler: app.authenticatePage,
+		},
+			(connection:SocketStream, request:FastifyRequest) => {
+				gameController(connection, request)
+			}
+	)	
+}
 
-//game schema
-export const GameSchema = {
-    type:'object',
-    required: ['id', 'player1', 'player2', 'score1', 'score2'],
-    properties: {
-        id: {type: 'number'},
-        player1: {type: 'string'},
-        player2:  {type: 'string'},
-        score1: {type: 'number'},
-        score2: {type: 'number'},
-        diff: {type: 'number'},
-        created_at: { type: 'string' },
-        updated_at: { type: 'string' },
-    },
-};
-
-export const CreateGameSchema = {
-    type: 'object',
-    required: ['player1', 'player2', 'score1', 'score2'],
-    properties: {
-        player1: {type: 'string'},
-        player2:  {type: 'string'},
-        score1: {type: 'number'},
-        score2: {type: 'number'},
-        diff: {type: 'number'},        
-    },
-};
-
-export const gameRoutes = async(app: FastifyInstance)=>{
-    
-   //CREATE
-    app.post<{ Body: Omit<Game, 'id' | 'created_at' | 'updated_at'> }>('/games', {
-        schema: {
-            tags: ['Games'],
-            body: CreateGameSchema,
-            response: {
-                201: GameSchema,
-                400: { type: 'object', properties: { error: { type: 'string' } } },
-                409: { type: 'object', properties: { error: { type: 'string' } } },
-            }
-        }
-    }, createGameController);
-
-    //READ GAME by
-    app.get<{ Params: { username: string } }>('/play-online', {
-            preHandler: app.authenticateApi,
-        }
-    }
