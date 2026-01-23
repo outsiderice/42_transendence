badf4c3b1f349a8467990f710c65cd1e44cb2f94
outsiderice <arantxamagnelli@gmail.com>
2026-01-23 05:52:02 +0100

    removed old token

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { jwtDecode } from 'jwt-decode'
 
-const jwt = localStorage.getItem('accessToken');
 
-console.log(jwt);
 
-const decodedHeader = jwtDecode(jwt);
-console.log(decodedHeader);
+//const decodedHeader = jwtDecode(jwt);
+//console.log(decodedHeader);
 
 </script>
 
