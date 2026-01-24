8b298ed4f509694ab29f340f39ef84465233c068
outsiderice <arantxamagnelli@gmail.com>
2026-01-24 18:32:49 +0100

    nginx up

--- proxy/tools/entrypoint.sh
+++ proxy/tools/entrypoint.sh
@@ -5,14 +5,13 @@ set -e
 CERT=/certs/localhost.crt
 KEY=/certs/localhost.key
 
-if [ ! -f "$CERT" ]; then
+if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
   echo "Generating self-signed cert..."
   openssl req -x509 -nodes -days 365 \
     -newkey rsa:2048 \
     -keyout "$KEY" \
     -out "$CERT" \
     -subj "/CN=localhost"
-	-addtext "subjectAltName=DNS:localhost"
 fi
 
 exec nginx -g "daemon off;"
