ba0b6bf0bd57f71f6f3b52f0f54964dde359b8b8
outsiderice <arantxamagnelli@gmail.com>
2026-01-28 12:57:07 +0100

    terms and privacy text added

--- frontend/src/views/PrivacyPolicy.vue
+++ frontend/src/views/PrivacyPolicy.vue
@@ -2,5 +2,21 @@
 </script>
 
 <template>
-	<h3> Privacy policy </h3>
+<div class="privacy-container">
+    <h3>Privacy Policy</h3>
+
+    <p>
+      When you sign up, we collect your email address and a username.
+    </p>
+
+    <p>
+      Your email is used only to manage your account and allow you to sign in.
+      We do not sell, share, or use your data for advertising or any other purpose.
+    </p>
+
+    <p>
+      If you delete your account, your email and associated data are permanently
+      removed from our database.
+    </p>
+  </div>	
 </template>
