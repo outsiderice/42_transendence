29d29937a1ac2426fb83c9f41de1f7ce071499a9
Jose casa <josemiguel.profe@gmail.com>
2026-01-31 11:10:03 +0100

    some errors

--- backend/src/modules/users/Controllers/userControllers.ts
+++ backend/src/modules/users/Controllers/userControllers.ts
@@ -272,32 +272,33 @@ export const updateUserController = async (
       return reply.status(404).send({ error: 'Usuario no encontrado' });
     }
 
-    const allowedFields: (keyof User)[] = ['username', 'email', 'password', 'nickname', 'avatar'];
+    const allowedFields: (keyof User)[] = ['username', 'email', 'password','oldpassword', 'nickname', 'avatar'];
     const fieldsToUpdate: Partial<User> = {};
-
+    const passwordtocheck: string = allowedFields.includes('oldpassword') && request.body.oldpassword ? request.body.oldpassword : '';
     for (const key of allowedFields) {
       const value = request.body[key];
       if (value !== undefined) {
-        // Cast seguro para evitar error de TypeScript
-        (fieldsToUpdate as any)[key] = value;
+        if (key === 'password') {
+          if (typeof value !== 'string' || value.length < 8 ) {
+            return reply.status(400).send({ error: 'La contraseña debe tener al menos 8 caracteres' });
+          }
+          if (!passwordtocheck || passwordtocheck.trim() === '') {
+            return reply.status(400).send({ error: 'La contraseña antigua es requerida para cambiar la contraseña' });
+          }
+          const isOldPasswordCorrect = bcrypt.compareSync(passwordtocheck, existingUser.password);
+          if (!isOldPasswordCorrect) {
+            return reply.status(401).send({ error: 'La contraseña antigua es incorrecta' });
+          }
+          const saltRounds = 10;
+          const password: string = value as string;
+          const hash = bcrypt.hashSync(password, saltRounds);
+          (fieldsToUpdate as any)[key] = hash;
+        } else {
+          (fieldsToUpdate as any)[key] = value;
+        }
       }
     }
 
-    const updatedUser = await DBClient.updateUser(id, fieldsToUpdate);
-
-    if (!updatedUser) {
-      return reply.status(500).send({ error: 'No se pudo actualizar el usuario' });
-    }
-
-    reply.status(200).send(updatedUser);
-  } catch (error) {
-    console.error('Error in updateUserController:', error);
-    reply.status(500).send({
-      error: 'Error al actualizar usuario',
-      details: error instanceof Error ? error.message : String(error),
-    });
-  }
-};
 
 /**
  * DELETE /users/:id - Eliminar usuario
