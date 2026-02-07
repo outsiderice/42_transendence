4825ca28d069d64d37a6cd01af9a22a599ea1dd1
A.M.S <99796232+outsiderice@users.noreply.github.com>
2026-02-07 11:31:07 +0000

    comments

--- backend/src/services/avatarService.ts
+++ backend/src/services/avatarService.ts
@@ -1,18 +1,23 @@
 
-export async function getAvatar()
+//is passed file path from a route controller and returns image stream
+export async function getAvatar(path)
 {}
 
-// receives image from the post avatar controller
-export async function uploadAvatar()
+//is passed the image stream (or whatever) and it's file path from a route controller and returns bool
+export async function uploadAvatar(path)
 {
     // if id avatar route isn't empty delete image at /public/avatars/id-avatar
     // save image to /public/avatars/id-avatar
     // save possibly new route to db
 }
 
+//is passed the file path from a route controller and returns bool
 export async function deleteAvatar()
 {}
 
+//is passed the image stream (or whatever) from a route controller and returns bool
 export async function validateAvatar()
-{}
+{
+
+}
 
