2ff1ef8a4f9e5030cbe6d1f48bd6e94b7bd7b7e1
A.M.S <99796232+outsiderice@users.noreply.github.com>
2026-02-07 11:18:30 +0000

    moved avatar hadnling

--- backend/src/modules/users/avatarHandler.ts
+++
@@ -1,4 +0,0 @@
-// receive image from update user controller
-// if id avatar route isn't empty delete image at /public/avatars/id-avatar
-// save image to /public/avatars/id-avatar
-// save possibly new route to db
---
+++ backend/src/services/avatarService.ts
@@ -0,0 +1,18 @@
+
+export async function getAvatar()
+{}
+
+// receives image from the post avatar controller
+export async function uploadAvatar()
+{
+    // if id avatar route isn't empty delete image at /public/avatars/id-avatar
+    // save image to /public/avatars/id-avatar
+    // save possibly new route to db
+}
+
+export async function deleteAvatar()
+{}
+
+export async function validateAvatar()
+{}
+
