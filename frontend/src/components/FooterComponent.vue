844fe860421f35b27704b8ffd9535a374e660cff
Andrea Martins <andmart2@cbr3s1.42barcelona.com>
2026-02-10 10:44:51 +0100

    test components deleted from footer

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -6,8 +6,6 @@ import LinkComponent from '../components/LinkComponent.vue'
 	<footer class="fixed bottom-0 block w-full mb-[2rem] flex justify-center gap-[2rem]">
 	<LinkComponent href="/terms_of_service" label="Terms of service" />
 	<LinkComponent href="/privacy_policy" label="privacy_policy" />
-	<LinkComponent href="/test" label="test" />
-	<LinkComponent href="/test_2" label="test 2" />
 	<LinkComponent href="/sign_up" label="sign up" />
 	<LinkComponent href="/edit_profile" label="edit profile" />
 </footer>
