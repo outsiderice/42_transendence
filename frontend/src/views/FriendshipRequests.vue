3d2d30e93bd27cef4e3f97a51fedb563b2ef7c95
outsiderice <arantxamagnelli@gmail.com>
2026-02-10 01:41:25 +0100

    filling friendshiprequests view

--- frontend/src/views/FriendshipRequests.vue
+++ frontend/src/views/FriendshipRequests.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
+import UserRequestCard from "@/components/UserRequestCard.vue";
+import { useSessionStore } from '@/state/user_session.ts'
+import { ref, reactive, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { useOnlineUsersStore } from '@/state/online_users.ts'
+
+const session = useSessionStore();
+const router = useRouter();
+
+
+const onlineUsers = useOnlineUsersStore();
+
+const users = reactive
+<
+	{
+		id: number,
+		name: string, 
+		nick: string, 
+		online: boolean, 
+		profilePic?: string
+	}[]
+> ([]);
+
 </script>
 
 <template>
 <section class="max-w-[40rem] p-[1rem] m-auto flex flex-col gap-[2rem]">
+	<p>friendship requests.</p>
 </section>
 </template>
